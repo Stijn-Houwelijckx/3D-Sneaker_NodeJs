4897eff73a5317c48d1472c0ff0ebd4f6f0f9228
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-11-17 22:24:43 +0100

    feat: get all users

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -3,7 +3,7 @@ const User = require("../../../models/api/v1/User");
 const create = (req, res) => {
   const { name, email, password } = req.body.user;
 
-  // Create a new message
+  // Create a new user
   const user = new User({ name: name, email: email, password: password });
   user
     .save()
@@ -24,6 +24,36 @@ const create = (req, res) => {
     });
 };
 
+// Get all users
+const index = async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(204).json({
+        status: "success",
+        message: "No users found",
+        data: {
+          users: [],
+        },
+      });
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: {
+          users: users,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Unable to fetch users",
+      error: err.message,
+    });
+  }
+};
+
 module.exports = {
   create,
+  index,
 };
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../../../controllers/api/v1/users");
 
 router.post("/", userController.create);
+router.get("/", userController.index);
 
 module.exports = router;
