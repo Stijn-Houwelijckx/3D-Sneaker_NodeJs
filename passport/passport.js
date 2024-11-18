16354c94e3d7f3cb41716eff7141ab64065ee792
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-11-19 00:26:56 +0100

    feat: added webtoken strategy (jwt)

--- passport/passport.js
+++ passport/passport.js
@@ -7,3 +7,27 @@ passport.use(User.createStrategy());
 // Serialize user data for sessions
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// WEBTOKEN STRATEGY (JWT)
+var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "TemporarySecretKey";
+passport.use(
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      // FIX: Use async/await instead of callback
+      const user = await User.findOne({ _id: jwt_payload.uid });
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+module.exports = passport;
