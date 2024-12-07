753e60df3ad7a5ae0250913f8463dfdbe1a41724
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-12-07 15:37:49 +0100

    feat: Change password

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../../../controllers/api/v1/users");
 const authController = require("../../../controllers/auth");
+const passport = require("../../../passport/passport");
 
 router.get("/", userController.index);
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.put(
+  "/change-password",
+  passport.authenticate("jwt", { session: false }),
+  authController.changePassword
+);
 
 module.exports = router;
